c1cef24a79d6a741d0f4151e9348b4784cee23c7
ray <564772717@qq.com>
2018-05-26 17:59:16 +0800

    木棒拼图

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
-
+#include <set>
 using namespace std;
 
 int main(){
-	cout << "Hello World!" << endl;
-	cout << "test" << endl;
-	cout << "修复了一个Bug" << endl;
-	cout << "It's a new version!" << endl;
-
+	int n;
+	cin >> n;
+	int sum=0,maxlen=0;
+	set<int> arr_len;
+	for(int i=0; i<n; i++)
+	{
+		int type,len;
+		cin >> type >> len;
+		if(type==1){
+			arr_len.insert(len);
+			if(maxlen<len)
+				maxlen=len;
+			sum+=len;
+		}
+		else if(type==2){
+			arr_len.erase(len);
+			sum-=len;
+			if(maxlen==len)
+				maxlen=*arr_len.begin();
+		}
+		if(sum>2*maxlen)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+	}
+	return 0;
 }
